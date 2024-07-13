b18d348cab577bbd1c8c6692a367d8faf3bba1aa
Yadhukrishnan <ybc2204@gmail.com>
2024-07-14 02:40:56 +0530

    transaction update

--- brics/src/components/Transaction.jsx
+++ brics/src/components/Transaction.jsx
@@ -1,20 +1,41 @@
-import React, { useState } from "react";
+import  { useEffect, useRef, useState } from "react";
 import { Search } from "@mui/icons-material";
-
+import axios from 'axios';
 const Transaction = () => {
   const [query, setQuery] = useState("");
   const [selectedUser, setSelectedUser] = useState(null); // State to track selected user
+  const userContainerRef = useRef(null);
+  const [mail,setmail] =useState("");
+  const [amt,setamt] =useState("");
+  const[error,setError] = useState("");
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (userContainerRef.current && !userContainerRef.current.contains(event.target)) {
+        setSelectedUser(null);
+      }
+    };
 
-  const users = ["Person1", "Person2", "Person3", "Person4"];
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+
+  const users = ["anjanakj000@gmail.com", "parveen123@gmail.com", "jijojohny13@gmail.com", "anson210@gmail.com"];
 
   const renderUsers = () => {
+    
     return users.map((user) => (
+      
       <button
         key={user}
-        className={`user-item bg-slate-800 rounded-md hover:border-b hover:scale-105 hover:cursor-pointer ${
-          selectedUser === user ? "bg-slate-500  text-white" : ""
+        className={`user-item bg-slate-800 p-3 rounded-md hover:border-b hover:scale-105 hover:cursor-pointer ${
+          selectedUser === user ? "bg-slate-500  text-white scale-105 border-b" : ""
         }`}
-        onClick={() => setSelectedUser(user)}
+        onClick={() => {
+          setSelectedUser(user);
+          setmail(user);}}
       >
         {user}
       </button>
@@ -22,13 +43,31 @@ const Transaction = () => {
   };
 
   const proceedButton = (
-    <button className="proceed-button bg-blue-500 text-white px-4 py-2 rounded-md mt-2">
+    <button className="proceed-button bg-blue-500 text-white  px-5 mx-auto py-2 rounded-md mt-2 w-min">
       Proceed
     </button>
   );
 
+  const handleProceed = async (e) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem('token');
+     
+     console.log(token)
+      const response = await axios.post('http://localhost:3000/api/token/transaction', {"email":mail},{"amount":amt}, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      });
+      // Handle response
+    } catch (error) {
+      console.error('Buy Token failed:', error);
+      setError('Buy Token failed. Please try again later.');
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-4 px-3 py-5 mt-10 border-2 rounded-[20px] border-white bg-gray-900 mx-auto w-[40%]">
+    <div className="flex flex-col gap-4 px-4 py-5 mt-10 border-2 rounded-[20px] border-white bg-gray-900 mx-auto w-[50%]">
       <div className="font-semibold uppercase text-2xl text-center">
         Transaction
       </div>
@@ -45,9 +84,9 @@ const Transaction = () => {
           />
         </div>
       </div>
-      <div className="w-3/4 flex flex-col gap-2 px-4 py-2 rounded-xl">
+      <div className=" flex flex-col gap-2 px-4 py-2 rounded-xl"  ref={userContainerRef}>
         <div className="text-xl font-semibold">Contacts</div>
-        <div className="flex flex-col gap-2">{renderUsers()}</div>
+        <div className="flex flex-col gap-2 font-semibold">{renderUsers()}</div>
         {selectedUser && proceedButton}
       </div>
     </div>
