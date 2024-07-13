50119bc3d26bab1aeccbce281b66a437de3aa711
Yadhukrishnan <ybc2204@gmail.com>
2024-07-14 04:11:44 +0530

    Transaction integration

--- brics/src/components/Transaction.jsx
+++ brics/src/components/Transaction.jsx
@@ -1,6 +1,8 @@
 import  { useEffect, useRef, useState } from "react";
 import { Search } from "@mui/icons-material";
 import axios from 'axios';
+import CloseIcon from '@mui/icons-material/Close';
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 const Transaction = () => {
   const [query, setQuery] = useState("");
   const [selectedUser, setSelectedUser] = useState(null); // State to track selected user
@@ -56,16 +58,19 @@ const Transaction = () => {
  
 
   const handleProceed = async (e) => {
-    e.preventDefault();
-    try {
+    //e.preventDefault();
+  console.log(amt);
+   console.log(mail); 
+  try {
       const token = localStorage.getItem('token');
      
      console.log(token)
-      const response = await axios.post('http://localhost:3000/api/token/transaction', {"email":mail},{"amount":amt}, {
-        headers: {
-          'Authorization': `Bearer ${token}`,
+      const response = await axios.post('http://localhost:3000/api/token/transactions', {"email":mail,"amount":amt},
+       { headers: {
+          'authorization': `Bearer ${token}`,
         }
       });
+      console.log(response)
       // Handle response
     } catch (error) {
       console.error('Buy Token failed:', error);
@@ -76,7 +81,7 @@ const Transaction = () => {
   return (
 
     <div className={`relative ${proceed ? "bg-black bg-opacity-50" : ""}`}>
-      <div className="flex flex-col gap-4 px-3 py-5 mt-10 border-2 rounded-[20px] border-white bg-gray-900 mx-auto w-[40%]">
+      <div className="flex flex-col gap-4 px-4 py-5 mt-10 border-2 rounded-[20px] border-white bg-gray-900 mx-auto w-[50%]">
         <div className="font-semibold uppercase text-2xl text-center">
           Transaction
         </div>
@@ -93,9 +98,9 @@ const Transaction = () => {
             />
           </div>
         </div>
-        <div className="w-3/4 flex flex-col gap-2 px-4 py-2 rounded-xl">
+        <div className="w-full flex flex-col gap-2 px-4 py-2 rounded-xl">
           <div className="text-xl font-semibold">Contacts</div>
-          <div className="flex flex-col gap-2">{renderUsers()}</div>
+          <div className="flex flex-col gap-2 font-semibold">{renderUsers()}</div>
           {selectedUser && (
             <button
               className="proceed-button bg-blue-500 text-white px-4 py-2 rounded-md mt-2"
@@ -108,14 +113,19 @@ const Transaction = () => {
       </div>
       {proceed && (
         <div className="bg-black bg-opacity-50 absolute inset-0 flex items-center justify-center ">
-          <div className="btoken-container">
-            <div className="bg-gray-800" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '110px', height: '60vh', borderRadius: '10px' }}>
-              <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label center-item font-bold">Enter Amount</label>
+          <div className="btoken-container ">
+          
+            <div className="bg-gray-800 relative" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '110px', height: '60vh', borderRadius: '10px' }}>
+            <div className="absolute top-2 right-4" onClick={()=>setProceed(false)}><CloseIcon fontSize="large" color=""/></div>
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label center-item font-bold text-xl">Enter Amount</label>
                 <div className="join">
                   <div className="grow">
                     <div className='center-item drop-item'>
-                      <input id="amount" name="amount" className="center-item input join-item ip-padding" type="text" placeholder="1.00" />
+                      <input id="amount" name="amount" className="center-item input join-item ip-padding" type="text" placeholder="" 
+                        value={amt}
+                        onChange={(e) => setamt(e.target.value)}
+                      />
                     </div>
                   </div>
                   <div className='center-item drop-item'>
@@ -127,7 +137,8 @@ const Transaction = () => {
                   </div>
                 </div>
                 <div className='center-item'>
-                  <button type="button" className="px-3 py-2 border-2 border-green-700 text-green-600 rounded-lg hover:bg-green-600 hover:text-white font-bold">Buy Now</button>
+                  <button type="button" className=" flex gap-1 px-3 py-2 border-2 border-green-700 text-green-600 rounded-lg hover:bg-green-600 hover:text-white font-bold"
+                  onClick={()=> handleProceed()}>Send<ArrowOutwardIcon/></button>
                 </div>
               </div>
             </div>
@@ -135,28 +146,7 @@ const Transaction = () => {
         </div>
       )}
 
-    <div className="flex flex-col gap-4 px-4 py-5 mt-10 border-2 rounded-[20px] border-white bg-gray-900 mx-auto w-[50%]">
-      <div className="font-semibold uppercase text-2xl text-center">
-        Transaction
-      </div>
-      <div>
-        <div className="input-box flex items-center gap-1">
-          <Search color="primary" />
-          <input
-            type="search"
-            name="search-form"
-            id="search-form"
-            className="search-input p-2 text-black rounded-xl bg-gray-300 placeholder:text-black w-full"
-            onChange={(e) => setQuery(e.target.value)}
-            placeholder="Search user"
-          />
-        </div>
-      </div>
-      <div className=" flex flex-col gap-2 px-4 py-2 rounded-xl"  ref={userContainerRef}>
-        <div className="text-xl font-semibold">Contacts</div>
-        <div className="flex flex-col gap-2 font-semibold">{renderUsers()}</div>
-        {selectedUser && proceedButton}
-      </div>
+  
 
     </div>
   );
