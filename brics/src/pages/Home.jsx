68ab3421766aa27e037791ba395d9cb798e1ca55
JijoJohny <147225080+JijoJohny@users.noreply.github.com>
2024-07-14 00:43:23 +0530

    Nav Bar

--- brics/src/pages/Home.jsx
+++ brics/src/pages/Home.jsx
@@ -12,63 +12,61 @@ import MultipleStopIcon from '@mui/icons-material/MultipleStop';
 import HistoryIcon from '@mui/icons-material/History';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 
-
 const Home = () => {
  
-    const sideclass ="flex gap-2 hover:underline hover:cursor-pointer"
+    const sideclass = "flex items-center gap-2 p-3 bg-gray-800 hover:bg-gray-700 rounded-md hover:underline hover:cursor-pointer"
 
   return (
-    <div className=' h-screen flex'>
-      <div className='bg-gradient-to-br from-blue-900 to-slate-800 w-[20%] flex flex-col gap-3'>
-<div>LOGO</div>
-<div className='flex flex-col gap-3'>
-   <div className={sideclass}>
-     <BalanceIcon/>
-     <div>Balance</div>
-   </div>
-   <div className={sideclass}>
-   <CurrencyExchangeIcon/>
-     <div>BuyTokens</div>
-   </div>
-   <div className={sideclass}>
-     <MultipleStopIcon/>
-     <div>Transaction</div>
-   </div>
-   <div className={sideclass}>
-     <HistoryIcon/>
-     <div>History</div>
-   </div>
-   <div className={sideclass}>
-     <TrendingUpIcon/>
-     <div>Stock</div>
-   </div>
-</div>
-<div className=' absolute bottom-0'>Footer</div>
+    <div className='h-screen flex'>
+      <div className='bg-gradient-to-br from-blue-900 to-slate-800 w-full sm:w-[20%] flex flex-col gap-3 p-3'>
+        <div className='text-center text-white text-xl mb-3'>LOGO</div>
+        <div className='flex flex-col gap-3'>
+          <div className={sideclass}>
+            <BalanceIcon />
+            <div>Balance</div>
+          </div>
+          <div className={sideclass}>
+            <CurrencyExchangeIcon />
+            <div>BuyTokens</div>
+          </div>
+          <div className={sideclass}>
+            <MultipleStopIcon />
+            <div>Transaction</div>
+          </div>
+          <div className={sideclass}>
+            <HistoryIcon />
+            <div>History</div>
+          </div>
+          <div className={sideclass}>
+            <TrendingUpIcon />
+            <div>Stock</div>
+          </div>
+        </div>
+        <div className='mt-auto text-white text-center'>Footer</div>
       </div>
-      <div className='flex flex-col text-white w-[80%] ' id='contents'>
-       <div>
-        <Balance/>
-       </div>
-       <div>
-         <BuyTokens/>
-       </div>
-       <div>
-         <Transaction/>
-       </div>
-       <div>
-        <History/>
-       </div>
-       <div>
-        <Stock/>
-       </div>
-       <div>
-        <Paypeer/>
-       </div>
+      <div className='flex flex-col text-white w-full sm:w-[80%] p-5' id='contents'>
+        <div>
+          <Balance />
+        </div>
+        <div>
+          <BuyTokens />
+        </div>
+        <div>
+          <Transaction />
+        </div>
+        <div>
+          <History />
+        </div>
+        <div>
+          <Stock />
+        </div>
+        <div>
+          <Paypeer />
+        </div>
       </div>
-     
-     
     </div>
   )
 }
 
 export default Home
+
