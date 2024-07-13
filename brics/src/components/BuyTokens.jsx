925bad8df8afee6523692086b5fa65398fec1f98
Anson Antony <ansonantony89@gmail.com>
2024-07-13 15:04:22 +0530

    Transaction History and Buy Tokens component

--- brics/src/components/BuyTokens.jsx
+++ brics/src/components/BuyTokens.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const BuyTokens = () => {
+const BuyToken = () => {
   return (
-    <div>BuyTokens</div>
-  )
-}
+    <div className="btoken-container">
+    <div className="card" style={{ display:'flex',alignItems:'center',justifyContent:'center',backgroundColor: '#7563ec', padding: '20px', height:'50vh',borderRadius: '10px' }}>
+      
+        <div className="mb-3">
+          <div className="dropdown drop-item center-item">
+            <button name = "country"  className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Select Country
+            </button>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" onClick={()=>{country.value="Brazil"}}>Brazil</a></li>
+              <li><a className="dropdown-item">India</a></li>
+              <li><a className="dropdown-item">United Arab Emirates</a></li>
+              <li><a className="dropdown-item">South Africa</a></li>
+              <li><a className="dropdown-item">Iran</a></li>
+              <li><a className="dropdown-item">Egypt</a></li>
+              <li><a className="dropdown-item">Ethopia</a></li>
+              <li><a className="dropdown-item">Russia</a></li>
+              <li><a className="dropdown-item">China</a></li>
+            </ul>
+          </div>
+          <label htmlFor="exampleInputEmail1" className="form-label center-item">Enter Amount</label>
+          <div className="join">
+            <div className="grow">
+              <div className='center-item drop-item'>
+                <input id="amount" name="amount" className="input-bordered center-item input join-item" style={{ width: '50%' }} type="text" placeholder="0.01" />
+              </div>
+            </div>
+            <div className='center-item drop-item'>
+            <select id="asset" name="asset" className="select-bordered select join-item" style={{ width: '50%' }}>
+              <option value="" disabled="">Select Asset</option>
+              <option value="native">XLM</option>
+            </select></div>
+          </div>
+          {/* <input style={{width:'25%'}} className="input-field" type="n" placeholder="Email" /> */}
+          <div className='center-item'>
+          <button type="button" className="btn btn-outline-danger center-item">Buy Now</button>
+          </div>
+        </div>
+     
+    </div>
+    </div>
+  );
+};
 
-export default BuyTokens
+export default BuyToken;
--- brics/src/components/History.jsx
+++ brics/src/components/History.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const History = () => {
   return (
-    <div>History</div>
-  )
-}
+    <>
+        
+        <div className="btoken-container">
+        <div class="overflow-x-auto">
+          <h3 className='drop-item'>Transaction History</h3> 
+       
+        <table class="table-compact table ">
+          <thead>
+            <tr>
+              <th>Amount</th>
+               <th>Asset</th> 
+               <th>Direction</th>
+                <th>Protocol</th>
+                 <th>Status</th> 
+                 <th>Date</th> 
+                 <th>More Info</th> 
+                 <th>Actions</th>
+            </tr></thead> 
+        <tbody>
+        <tr>
+              <td>0</td>
+               <td>XLM</td> 
+               <td>-</td>
+                <td>-</td>
+                 <td>-</td> 
+                 <td>12/02/11</td> 
+                 <td>-</td> 
+                 <td>-</td>
+            </tr>
+            <tr>
+              <td>0</td>
+               <td>XLM</td> 
+               <td>-</td>
+                <td>-</td>
+                 <td>-</td> 
+                 <td>12/02/11</td> 
+                 <td>-</td> 
+                 <td>-</td>
+            </tr>
+          </tbody>
+          
+          </table> 
+          
+          </div>
+          
+          </div>
+          {/* <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table> */}
+    </>
+  );
+};
 
-export default History
+export default History;
