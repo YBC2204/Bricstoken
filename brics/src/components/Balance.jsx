7734cbae1ec30cf3ed5b951748c04316da230ff1
Yadhukrishnan <ybc2204@gmail.com>
2024-07-14 08:40:30 +0530

    stock view

--- brics/src/components/Balance.jsx
+++ brics/src/components/Balance.jsx
@@ -1,15 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import AddIcon from '@mui/icons-material/Add';
+import LoopIcon from '@mui/icons-material/Loop';
+import axios from 'axios';
 
 const Balance = () => {
+  const [balbric, setBalbric] = useState(null);
+  const [xml, setXml] = useState(null);
+  const [error, setError] = useState('');
+
+  const balancefn = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      console.log(token);
+      const response = await axios.get('http://localhost:3000/api/token/balance/bric', {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      });
+      const balance = response.data.bricBalance.balance;
+      setBalbric(balance);
+      console.log(balance);
+    } catch (error) {
+      console.error('Balance failed:', error);
+      setError('Balance failed. Please try again later.');
+    }
+  };
+
+  const xmlbalancefn = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      console.log(token);
+      const response = await axios.get('http://localhost:3000/api/token/balance/xml', {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      });
+      const xmlbalance = response.data.xmlBalance.balance;
+      setXml(xmlbalance);
+      console.log(xmlbalance);
+    } catch (error) {
+      console.error('Balance failed:', error);
+      setError('Balance failed. Please try again later.');
+    }
+  }; 
+
   return (
     
         <div className="flex items-center justify-center h-screen  ">
           <div className="flex flex-col items-center justify-center bg-gray-900 p-4 rounded-lg shadow-lg w-[500px] h-[550] border border-white">
           
         <div className="w-full text-center text-white mb-4 flex flex-col">
-              <h1 className="text-xl font-bold">Balance</h1>
+              <h1 className="text-2xl font-bold">Balance</h1>
               <h2 className="text-sm text-gray-400">Tokens</h2>
              <div className='flex  font-semibold hover:cursor-pointer'  
              onClick={() => {
